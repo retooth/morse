8a88b9c6ae56e81f04c7d2b18174fe2dc81c7646
Fabian Vallon <fabian.vallon@gmx.de>
2014-06-10 09:15:27 -0700

    moved installer to views, closed-thread security fix

--- morse/views.py
+++ morse/views.py
@@ -54,6 +54,45 @@ login_manager.login_view = 'login'
 def load_user(id):
     return User.query.get(int(id))
 
+@app.route('/install')
+def install ():
+    """ 
+    Creates base tables / installs default config
+    :rtype: html
+    """
+    db.create_all()
+    
+    # !important: dont't change the order
+    gadmin = Group("Administrators", True, True, True, True, 4)
+    gmods = Group("Moderators", False, True, True, True, 3)
+    gregistered = Group("Registered", False, False, False, False, 2)
+    gguests = Group("Guests", False, False, False, False, 1)
+    
+    db.session.add(gadmin)
+    db.session.add(gmods)
+    db.session.add(gregistered)
+    db.session.add(gguests)
+
+    admin = User("admin", "admin", "root@localhost")
+    db.session.add(admin)
+    db.session.commit()
+
+    rel = GroupMember(admin.id,gadmin.id)
+    mode1 = GroupMode(0, gadmin.id, True, True, True) 
+    mode2 = GroupMode(0, gmods.id, True, True, True) 
+    mode3 = GroupMode(0, gregistered.id, True, True, True) 
+    mode4 = GroupMode(0, gguests.id, True, False, True) 
+
+    db.session.add(rel)
+    db.session.add(mode1)
+    db.session.add(mode2)
+    db.session.add(mode3)
+    db.session.add(mode4)
+
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
 @app.route('/register' , methods=['GET','POST'])
 def register():
     """ 
@@ -618,6 +657,9 @@ def post (topic_id):
 
     visible, readable, writable = get_my_boards(get_only_ids = True)
     if not topic.board_id in writable:
+        return "forbidden", 403
+
+    if topic.closed:
         return "forbidden", 403
 
     if not current_user.is_anonymous(): 
